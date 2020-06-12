5635cef4f2f5016b1ed229788241042248bd6d50
Rodrigo Díaz <rodinnig@gmail.com>
2020-06-11 22:36:16 -0400

    Solución completa

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -64,6 +64,11 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if (numero % 1 === 0){
+    return true;
+  } else {
+    return false;
+  }
  }
 
 function fizzBuzz(numero) {
@@ -88,6 +93,15 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero < 2){
+    return false;
+  } 
+  for (var i = 2; i < numero; i++){
+    if (numero % i === 0){
+    return false;
+    }
+    return true;
+  }
  }
 
 
